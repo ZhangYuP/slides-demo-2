132703d09b67a3047e08cd5da273baece67685ae
ZhangYuP <zyp5318@qq.com>
2018-12-10 03:31:18 +0800

    优化完全版轮播

--- main.js
+++ main.js
@@ -1,47 +1,69 @@
-let n
-初始化()
-setInterval(()=>{
-  makeLeave(getImage(n))
-    .one('transitionend',(e)=>{
-      makeEnter($(e.currentTarget))
-    })
-  makeCurrent(getImage(n+1))
-  n += 1
-},3000)
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
+makeFakeSlides()
+$slides.css({transform:'translateX(-800px)'})
+bindEvents()
+$(next).on('click', function(){
+  goToSlide(current+1)
+})
+$(previous).on('click',function(){
+  goToSlide(current-1)
+})
 
+let timer = setInterval(function(){
+  goToSlide(current+1)
+},2000)
+$('.container').on('mouseenter', function(){
+  window.clearInterval(timer)
+}).on('mouseleave', function(){
+  timer = setInterval(function(){
+    goToSlide(current+1)
+  },2000)
+})
 
+function makeFakeSlides(){
+  let $firstCopy = $images.eq(0).clone(true)
+  let $lastCopy = $images.eq($images.length - 1).clone(true)
 
-
-
-
-
-
-
-
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
+  $slides.append($firstCopy)
+  $slides.prepend($lastCopy)
 }
-function x(n){
-  if(n>6){
-    n = n%6
-    if(n===0){
-      n=6
-    }
+
+function bindEvents(){
+  $('#buttonWrapper').on('click', 'button', function(e){
+    let $button = $(e.currentTarget)
+    let index = $button.index()
+    goToSlide(index)
+  })
+}
+
+function goToSlide(index){
+  if(index>$buttons.length - 1){
+    index = 0
+  }else if(index<0){
+    index = $buttons.length - 1
   }
-  return n
-}
-function 初始化(){
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
-function makeCurrent($node){
-  $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-  return $node.removeClass('current enter').addClass('leave')
-}
-function makeEnter($node){
-  $node.removeClass('leave current').addClass('enter')
+  if(current === $buttons.length - 1 && index === 0){
+    $slides.css({transform:`translateX(${-($buttons.length+1)*800}px)`})
+      .one('transitionend', function(){
+        $slides.hide()
+          .offset()
+        $slides.css({transform:`translateX(${-(index+1)*800}px)`})
+          .show()
+      })
+  }else if(current === 0 && index === $buttons.length - 1){
+    $slides.css({transform:`translateX(0px)`})
+      .one('transitionend', function(){
+        $slides.hide()
+          .offset()
+        $slides.css({transform:`translateX(${-(index+1)*800}px)`})
+          .show()
+        })
+  }else{
+    $slides.css({transform:`translateX(${-(index+1)*800}px)`})
+  }
+  current = index
 }
